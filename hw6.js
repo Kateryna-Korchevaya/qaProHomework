a5618761ff6176285e0e1c2a59c1553ed88742c3
kateryna.korchevaya <68077632+Kateryna-Korchevaya@users.noreply.github.com>
2023-10-11 21:15:14 +0300

    Add files via upload

---
+++ hw6.js
@@ -0,0 +1,46 @@
+//1. Намалювати трикутник
+/*function drawTriangle(height, symbol) {
+    for (let i = 1; i <= height; i++) {
+        let row = '';
+        for (let j = 1; j <= i; j++) {
+            row += symbol;
+        }
+        console.log(row);
+    }
+}
+
+drawTriangle(5, '*');
+*/
+
+// 2. Вивести на екран суму чисел від 1 до 100 включно, які не кратні 3.
+/*let sum = 0;
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 !== 0) {
+        sum += i;
+    }
+}
+*/
+
+//3. Написати функцію pow(x,y) яка буде приймати 2 цисла, перше це число
+// яке ми будемо зводити у ступінь,друге у яку ступінь.
+// Наприклад pow(2,3) функція поверне значення 8.
+/*function pow(x, y) {
+    let result = 1;
+
+    for (let i = 0; i < y; i++) {
+        result *= x;
+    }
+
+    return result;
+}
+
+let result = pow(2, 3);
+console.log(result);
+
+ */
+
+
+
+
+
